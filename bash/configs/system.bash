b5030a00f040afce4733c564877e2e3b44c40581
Den Girnyk <den.girnyk@uvoteam.com>
2014-03-17 13:46:07 +0200

    Add alias for aptitude safe-upgrade

--- bash/configs/system.bash
+++ bash/configs/system.bash
@@ -26,6 +26,8 @@ alias llg='ll | grep '
 
 alias duh='du -h --max-depth=1'
 
+alias upgrade='aptitude update && aptitude safe-upgrade'
+
 # bash completion
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
