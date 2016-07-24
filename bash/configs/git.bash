dab4297b5a52f086e34db11837d55a10c5b4ed74
Den Girnyk <den.girnyk@gmail.com>
2016-07-24 21:29:35 +0300

    Remove 'go' alias (conflicts with go lang)

--- bash/configs/git.bash
+++ bash/configs/git.bash
@@ -4,6 +4,7 @@ alias ga='git add '
 alias gb='git branch '
 alias gc='git commit'
 alias gd='git diff'
-alias go='git checkout '
+# conflicts with go lang
+# alias go='git checkout '
 
 alias theirs="grep -lr  -color=none '<<<<<<<' . | xargs git checkout --theirs"
