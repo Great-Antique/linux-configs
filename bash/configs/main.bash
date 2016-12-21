fa27af4b8ae405a35165bb58487c991c164c9019
Den Girnyk <den.girnyk@uvoteam.com>
2016-12-21 08:46:55 +0200

    fixup! Change duh alias

--- bash/configs/main.bash
+++ bash/configs/main.bash
@@ -77,7 +77,7 @@ alias l='ls -CF'
 alias llg='ll | grep '
 
 #alias duh='du -h --max-depth=1'
-alias duh='du -sch *'
+alias duh='ls -a | grep -v "^\.\.$" | grep -v "^\.$" | xargs du -sch'
 
 alias upgrade='aptitude update && aptitude safe-upgrade'
 
