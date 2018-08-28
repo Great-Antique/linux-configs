2adafee8bd4f5bbac693dada904a5a78cdcea13f
Den Girnyk <den.girnyk@gmail.com>
2018-08-28 08:44:46 +0300

    Load bash_completion from local share first
    Fixes bash_completion for bash >= 4.2
    See:
    https://github.com/scop/bash-completion/commit/a47bd375bb0f95dc6d388d4097c420bddb72ae33
    https://github.com/scop/bash-completion/pull/210

--- bash/configs/main.bash
+++ bash/configs/main.bash
@@ -86,9 +86,11 @@ alias upgrade='aptitude update && aptitude safe-upgrade'
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
 if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
+  if [[ $PS1 && -f /usr/local/share/bash-completion/bash_completion ]]; then
+    . /usr/local/share/bash-completion/bash_completion
+  elif [[ $PS1 && -f /usr/share/bash-completion/bash_completion ]]; then
     . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
+  elif [[ -f /etc/bash_completion ]]; then
     . /etc/bash_completion
   fi
 fi
