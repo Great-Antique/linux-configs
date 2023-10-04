cd4987b2647b3b5001affe1032eb1f160cf1eb7b
Den G <den@Dens-MacBook-Air.local>
2023-10-04 10:37:36 +0300

    bash: Add bash completion for bash on mac installed by homebrew

--- bash/configs/main.bash
+++ bash/configs/main.bash
@@ -92,6 +92,8 @@ if ! shopt -oq posix; then
     . /usr/share/bash-completion/bash_completion
   elif [[ -f /etc/bash_completion ]]; then
     . /etc/bash_completion
+  elif [[ -f /opt/homebrew/etc/profile.d/bash_completion.sh ]]; then
+    . /opt/homebrew/etc/profile.d/bash_completion.sh
   fi
 fi
 
